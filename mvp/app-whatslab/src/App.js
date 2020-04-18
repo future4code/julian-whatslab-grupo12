be128dabe77ebefd6ccc12dda24c5453f7ffbc59
Ricardo Martins de Souza Timbó <rcardms@gmail.com>
2020-04-17 22:59:29 -0300

    adicionado styled-components ao app.js

--- mvp/app-whatslab/src/App.js
+++ mvp/app-whatslab/src/App.js
@@ -3,15 +3,26 @@ import logo from './logo.svg';
 import './App.css';
 import { ContainerConversa } from './components/ContainerConversa';
 import { AdicionarMensagem } from './components/AdicionarMensagem';
+import styled from "styled-components"
 
-
+const ContainerPrincipal = styled.div`
+  max-width: 600px;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  background-color: #e5ddd5;
+  border-width: 1px;
+  border-color: black;
+  border-style: solid;
+  flex: 1 1 0%;
+`
 
 function App() {
   return (
-    <div className="App">
+    <ContainerPrincipal>
       <ContainerConversa/>
       <AdicionarMensagem/>
-    </div>
+    </ContainerPrincipal>
   );
 }
 
